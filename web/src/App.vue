1e54c3a17281c7b8674c6ab82370e7d140a61240
bs f <304446350@qq.com>
2022-09-19 01:35:01 +0800

    9.19test git

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,30 +1,43 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <div>
+    hello,World
+    <div>从后端搞来的数据: {{ data1 }}</div>
+  </div>
   <router-view/>
 </template>
 
+
+<script>
+import $ from 'jquery';
+import { ref } from 'vue';
+
+export default {
+  name: "APP",
+  setup: ()=>{
+      let data1 = ref("");
+
+      $.ajax({
+        url: "http://127.0.0.1:3000/pk/getBotInfoList",
+        type: "get",
+        success: resp => {
+          console.log("1111");
+          console.log(resp);
+          data1.value = resp[0];
+        }
+      });
+
+
+      return {
+        data1
+      }
+  }
+}
+</script>
+
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+body {
+  background-image: url("@/assets/20.png");
+  background-size: cover;
 }
 </style>
