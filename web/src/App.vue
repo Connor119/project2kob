3c8ae440a8b2b7a762cf659b15971fe7bb49fe4c
bs f <304446350@qq.com>
2022-09-23 04:57:30 +0800

    register function

--- web/src/App.vue
+++ web/src/App.vue
@@ -32,6 +32,52 @@ export default {
         console.log(resp);
       }
     })
+
+    $.ajax({
+      url: "http://127.0.0.1:3000/user/account/info/",
+      type: "get",
+      headers: {
+        Authorization: "Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJkZjliMTJmNzI2MTM0YmZiOTc1MGQxODM1ZTY4Yjc1YiIsInN1YiI6IjIiLCJpc3MiOiJzZyIsImlhdCI6MTY2Mzg0MzUyMiwiZXhwIjoxNjY1MDUzMTIyfQ.9-d6yHzhrCCYVSAjEzv-D3IkHbk6QdDWj8R7GGV5Bo8",
+      },
+      success(resp) {
+        console.log(resp);
+      },
+      error(resp) {
+        console.log(resp);
+      }
+    });
+
+    $.ajax({
+      url: "http://localhost:3000/user/account/info/",
+      type: "get",
+      headers: {
+        Authorization:"Bearer "+"eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI1YjI3OWY2NjRkMWE0YzA5YjEyMWMzMGM3N2M4YzU3MyIsInN1YiI6IjIiLCJpc3MiOiJzZyIsImlhdCI6MTY2Mzg0MjM5MSwiZXhwIjoxNjY1MDUxOTkxfQ.IzgG2azZWnpy0lMX_bz6sZGKOWXVpnOhTewDtSOGZpY",
+      },
+      success(resp){
+        console.log(resp);
+      },
+      error(resp){
+        console.log(resp);
+      }
+    })
+
+    $.ajax({
+      url: "http://localhost:3000/user/account/register/",
+      type: "post",
+      data: {
+        username:'fbs1',
+        password:'119119',
+        confirmedPassword:'119118',
+      },
+      success(resp){
+        console.log(resp);
+      },
+      error(resp){
+        console.log(resp);
+      }
+    })
+
+
   }
 }
 </script>
